f046ba7a0d183ff5e5c104b71ac12f973bf311ab
Alexandre Serex <alexandre.serex@he-arc.ch>
2016-10-25 11:07:09 +0200

    Bases of fragments and initialization of first fragment done.

--- GeoSys/app/src/main/java/minimata/geosys/MainActivity.java
+++ GeoSys/app/src/main/java/minimata/geosys/MainActivity.java
@@ -1,13 +1,28 @@
 package minimata.geosys;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MapsFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Pushing MapsFragment
+        MapsFragment fragment = new MapsFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.activity_main, fragment);
+        ft.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
