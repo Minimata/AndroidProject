eb583aec64d72c310f77ca89b35e13a331cb5b13
Alexandre Serex <alexandre.serex@he-arc.ch>
2016-11-08 11:19:11 +0100

    Begun creating a BottomSheetBehaviour for setting new Alarms

--- GeoSys/app/src/main/java/minimata/geosys/MainActivity.java
+++ GeoSys/app/src/main/java/minimata/geosys/MainActivity.java
@@ -1,11 +1,16 @@
 package minimata.geosys;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.util.Log;
+import android.view.View;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 
 import minimata.geosys.dummy.DummyContent;
 
@@ -18,24 +23,64 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(findViewById(R.id.Maps_fragment_container) != null){
-            if(savedInstanceState != null){
+        if (findViewById(R.id.Maps_fragment_container) != null) {
+            if (savedInstanceState != null) {
                 return;
             }
 
             // Pushing MapsFragment
             MapsFragment mapsFragment = new MapsFragment();
-//            mapsFragment.setArguments(getIntent().getExtras());
+            //mapsFragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.Maps_fragment_container, mapsFragment).commit();
 
+            //Settings Fragment
             SettingFragment settingFragment = new SettingFragment();
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.Setting_fragment_container, settingFragment).commit();
 
+            //Alarm selection bottom sheet
+            View bottomSheet = findViewById(R.id.bottom_sheet);
+            final BottomSheetBehavior behavior = BottomSheetBehavior.from(bottomSheet);
+            bottomSheet.post(new Runnable() {
+                @Override
+                public void run() {
+                    behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            });
+            behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+                @Override
+                public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                    // React to state change
+                }
 
+                @Override
+                public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                    // React to dragging events
+                }
+            });
+
+            //New alarm button
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switch (behavior.getState()){
+                        case BottomSheetBehavior.STATE_COLLAPSED:
+                            behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                            break;
+
+                        case BottomSheetBehavior.STATE_EXPANDED:
+                            behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                            break;
+
+                        default:
+                            behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                            break;
+                    }
+                }
+            });
         }
-
     }
 
     @Override
@@ -45,6 +90,8 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onListFragmentInteraction(DummyContent.DummyItem dummyItem) {
-        Log.d("d", dummyItem.content);
+        View view = findViewById(R.id.activity_main);
+        Snackbar.make(view, dummyItem.content, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
